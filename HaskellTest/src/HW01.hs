949dc0d1e49157029a43364b74bba3da3d159cb1
pimtegelaar <pimtegelaar@gmail.com>
2015-09-04 21:43:51 +0200

    exercise 1: lastDigit implementation

--- HaskellTest/src/HW01.hs
+++ HaskellTest/src/HW01.hs
@@ -5,7 +5,7 @@ module HW01 where
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit = undefined
+lastDigit a = a `mod` 10
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
