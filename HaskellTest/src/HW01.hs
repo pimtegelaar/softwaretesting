b859eb640342547474e716a2001898fcdde55e80
pimtegelaar <pimtegelaar@gmail.com>
2015-09-04 23:13:39 +0200

    solutions for exercise 2

--- HaskellTest/src/HW01.hs
+++ HaskellTest/src/HW01.hs
@@ -14,8 +14,25 @@ dropLastDigit x = x `div` 10
 -- Exercise 2 -----------------------------------------
 
 toRevDigits :: Integer -> [Integer]
-toRevDigits = undefined
+toRevDigits 0 = [0]
+toRevDigits x = reverseDigits x
 
+reverseDigits :: Integral x => x -> [x]
+reverseDigits 0 = []
+reverseDigits x = x `mod` 10 : reverseDigits (x `div` 10)
+
+-- Slower version, but handles negatives
+toRevDigits2 :: Integer -> [Integer]
+toRevDigits2 x = digits(signum x * (read . reverse . show . abs $ x))
+   
+digits :: Integer -> [Integer]
+digits = map (read . (:[])) . show
+
+-- Concatenate the list of Integers back to an integer
+fromDigits :: [Integer] -> Integer
+fromDigits = foldl addDigit 0
+   where addDigit num d = 10*num + d
+      
 -- Exercise 3 -----------------------------------------
 
 -- Double every second number in a list starting on the left.
