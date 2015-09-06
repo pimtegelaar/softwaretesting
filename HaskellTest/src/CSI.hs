0a0af1413f2eadb9905653757881c897a4db5304
pimtegelaar <pimtegelaar@gmail.com>
2015-09-06 17:18:21 +0200

    half way there

--- HaskellTest/src/CSI.hs
+++ HaskellTest/src/CSI.hs
@@ -1,15 +1,30 @@
 module CSI where
 
+import Data.List
+
 data Boy = Matthew | Peter | Jack | Arnold | Carl 
            deriving (Eq,Show)
  
+boys :: [Boy]
 boys = [Matthew, Peter, Jack, Arnold, Carl]
 
 says :: Boy -> Boy -> Bool
-says = undefined
+says Jack b = b /= Matthew && b /= Peter
+says Arnold b = b == Matthew || b == Peter
+says Carl b = b /= Arnold
+says _ _ = True
 
 accusers :: Boy -> [Boy]
-accusers = undefined
+accusers Matthew = delete Matthew(delete Carl boys)
+accusers Peter = [Matthew, Jack]
+accusers _ = boys
+
+accuses :: Boy ->  Boy -> Bool
+accuses boy culprit = culprit `elem` accusers boy
+
+notlying :: Boy -> Boy -> Bool
+notlying boy culprit = accuses boy culprit
 
 guilty, honest :: [Boy]
 guilty = undefined
+honest = undefined
