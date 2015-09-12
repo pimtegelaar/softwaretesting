75d01ffaa955429498a1cde10f48e1072a839c96
weisf_000 <weisf_000@MICROHENK>
2015-09-12 14:13:46 +0200

    First version of all exercises week 2.

--- week2/src/HW01.hs
+++ week2/src/HW01.hs
@@ -1,6 +1,7 @@
 module Lab2 where
  
 import Data.List
+import Data.Char
 import System.Random
 
 -- Testing Functions
@@ -94,3 +95,48 @@ isDerangement xs ys = and [ x `elem` ys && (index x xs /= index x ys) | x <- xs 
 
 deran :: Int -> [[Int]]
 deran n = filter (\ p -> isDerangement p [0..n-1]) (perms [0..n-1])
+
+-- Exercise 4
+
+-- Helpers
+
+-- Remove spaces from a String
+
+removeSpaces :: String -> String
+removeSpaces [] = []
+removeSpaces [x] = [x]
+removeSpaces (x:xs) = filter (/=' ') (x:xs)
+
+-- Move first four characters to end of String
+-- Invalid output when x > 1 and x < 4
+
+moveCharacters :: String -> String
+moveCharacters [] = []
+moveCharacters [x] = [x]
+moveCharacters (x:xs) = drop 4 ((x:xs) ++ take 4 (x:xs))
+
+-- Convert non-numeric characters to numbers 
+-- Not correct yet, digitToInt is not the correct Function
+
+convertCharacters :: String -> String
+convertCharacters [] = []
+convertCharacters (x:xs) = show ( digitToInt (x)) ++ convertCharacters (xs)
+
+-- Prepare String for IBAN check
+
+convertToIBAN :: String ->  Integer
+convertToIBAN [] = 0
+convertToIBAN [x] = 0
+convertToIBAN (x:xs) = read (convertCharacters (moveCharacters (removeSpaces (x:xs)))) :: Integer
+
+-- Verify Integer against MOD 97
+
+checkMOD97 :: Integer -> Bool
+checkMOD97 x = x `mod` 97 == 1
+
+-- Verify whether String is valid IBAN value
+
+iban :: String -> Bool
+iban [] = False
+iban [x] = False
+iban (x:xs) = checkMOD97 (convertToIBAN (x:xs))
