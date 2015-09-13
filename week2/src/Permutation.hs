d25ae0be5d87bd1e0a05e4c4467724db945f8ac8
pimtegelaar <pimtegelaar@gmail.com>
2015-09-13 16:49:33 +0200

    added tests for all permutations

--- week2/src/Permutation.hs
+++ week2/src/Permutation.hs
@@ -20,13 +20,23 @@ tests = putStr (
     test [1, 2, 3] [3, 2] False ++ -- y smaller
     test [1, 2, 3] [3, 2, 3] False ++ -- y missing value from x
     test [3, 2, 3] [3, 2, 1] False ++ -- x missing value from y
+    test [1, 2, 3] [3, 2, 4] False ++ -- y has number not in x and x has number not in y
     test [1, 2, 3] [3, 2, 1] True ++ -- reversed
     test [1, 2, 3] [1, 2, 3] True ++ -- equal
     test [1, 2, 3] [3, 1, 2] True  -- different order
     )
     
-perms :: [a] ->[[a]]
+perms :: (Eq a, Show a) => [a] -> [[a]]
 perms [] = [[]]
 perms (x:xs) = concatMap (insrt x) (perms xs) where
   insrt x [] = [[x]]
   insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys)
+  
+testPerms :: (Eq a, Show a) => [a] -> [[a]] -> String
+testPerms [] _ = ""
+testPerms (_ : _) [] = ""
+testPerms x (y:ys) = test x y True ++ testPerms x ys
+
+testAllPerms :: (Show a, Eq a) => [a] -> IO ()
+testAllPerms x = putStr(testPerms x (perms x)) -- confirms that all permutations of a list are permutations...
+
