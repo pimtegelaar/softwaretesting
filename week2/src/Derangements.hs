73571c93874346bc6572bd69b7801ab5f5df4b03
verhoofstad <verhoofstad@users.noreply.github.com>
2015-09-13 19:14:07 +0200

    Update Derangements.hs

--- week2/src/Derangements.hs
+++ week2/src/Derangements.hs
@@ -1,9 +1,9 @@
 --
 -- Software Testing - Week 2
--- Erik Verhoofstad
 --
 -- Time spent:
 --    functions: 1 hour
+--    tests: 3 hours
 --
 module Derangements
 
@@ -22,3 +22,36 @@ isDerangement' (x:xs) (y:ys) = x /= y && isDerangement' xs ys
 
 deran :: Eq a => [a] -> [[a]]
 deran xs = [x | x <- perms xs, isDerangement x xs]
+
+-- Returns the number of possible derangements for a list of a given n elements.
+subfac :: Int -> Int
+subfac 0 = 0
+subfac 1 = 0
+subfac 2 = 1
+subfac n = (n-1) * (subfactorial (n-1) + subfactorial (n-2))
+
+-- Test isDerangement against all permutations of a given list
+-- and returns true if the number of succeeded tests matches the number of possible derangements for that list.
+testIsDerangement :: Eq a => [a] -> Bool
+testIsDerangement [] = False
+testIsDerangement xs = countSucceeded(testIsDerangement' xs) == subfac (length xs)
+
+-- Test isDerangement against all permutations of a given list
+testIsDerangement' :: Eq a => [a] -> [Bool]
+testIsDerangement' xs = map (isDerangement xs) (perms xs)  
+
+countSucceeded :: [Bool] -> Int
+countSucceeded [] = 0
+countSucceeded (x:xs) = (if x == True then 1 else 0) + countSucceeded xs
+
+test :: (Eq a, Show a) => [a] -> String
+test xs = show xs ++ " -> " ++ (if testIsDerangement xs then "Passed" else "Failed") ++ "\n"
+
+tests :: IO ()
+tests = putStr (
+    test [1] ++
+    test [1, 2] ++
+    test [1, 2, 3] ++
+    test [1, 2, 3, 4] ++
+    test [1, 2, 3, 4, 5]
+    )
