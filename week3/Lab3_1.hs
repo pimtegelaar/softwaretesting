3c27472e0f3ce6c24fbd8f4aa06eec8bed1ab9dd
pimtegelaar <pimtegelaar@gmail.com>
2015-09-20 15:48:01 +0200

    form cannot be a contradiction and a tautology at the same time

--- week3/Lab3_1.hs
+++ week3/Lab3_1.hs
@@ -26,9 +26,13 @@ entails :: Form -> Form -> Bool
 entails f g = all (\ v -> evl v (Impl f g)) (allVals f)
 
 --
--- Determines whether two given formulas are equivalant.
+-- Determines whether two given formulas are equivalent.
 --
 equiv :: Form -> Form -> Bool
 equiv f g = all (\v -> evl v f == evl v g) (allVals f)
 
+-- equivalence is independent of order
 testEquiv = quickCheckResult (\ x y -> equiv x y == equiv y x)
+
+-- form cannot be a contradiction and a tautology at the same time
+testConTaut = quickCheckResult (\ x -> not (contradiction x && tautology x))
