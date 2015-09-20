ab0561db5b3e9df7cdca662e8c67c6bd9a075863
weisf_000 <weisf_000@MICROHENK>
2015-09-20 16:32:53 +0200

    Cleaned up unnessary parts

--- week3/Lab3_4.hs
+++ week3/Lab3_4.hs
@@ -1,42 +1,8 @@
-module QuickCheckExample where
+module Lab3_4 where
+
 import Test.QuickCheck
 import Lecture3
 import Lab3_3
-
--- Test f commutativity with:
--- quickCheckResult (\ x y -> f x y == f y x)
-
-f :: String -> String -> String
-f a b = a ++ " " ++ b
-
--- Test g commutativity with:
--- quickCheckResult (\ x y -> g x y == g y x)
-
-g :: Int -> Int -> Int
-g a b = a + b
-
-data Subject  = Mathematics | Software_Testing | Hans_Subject deriving (Eq,Show,Enum)
-
-instance Arbitrary Subject where
-  arbitrary = elements [Mathematics, Software_Testing, Hans_Subject]
-
--- Test compatible commutativity with:
--- quickCheckResult (\ x y -> compatible x y == compatible y x)
-
-compatible :: Subject -> Subject -> Bool
-compatible x y
- | x == y                   = True
- | Mathematics `elem` [x,y] = True
- | otherwise                = False
-
--- Test badCompatible commutativity with:
--- quickCheckResult (\ x y -> badCompatible x y == badCompatible y x)
- 
-badCompatible :: Subject -> Subject -> Bool
-badCompatible x y
- | x == y                = True
- | x == Software_Testing = True
- | otherwise             = False
 
 -- Try with set of fixed formulas to compare truth tables between formulas 
 -- with CNF formulas.
@@ -56,6 +22,11 @@ form10 = Impl form8 form7
 instance Arbitrary Form where
   arbitrary = elements [form4, form5, form6, form7, form8, form9, form10]
 
--- Execute Function:
+checkQuick :: (Form -> [Valuation]) -> (Form -> Form) -> IO Result
+checkQuick g h = quickCheckResult (\ f -> g (h (f)) == g (f) )
+
+-- Execute following Function to compare truth tables against arbitrary formulas:
 -- quickCheckResult (\ f -> allVals (cnf (f)) == allVals (f) )
+
+-- With intermediate results:
 -- verboseCheck (\ f -> allVals (cnf (f)) == allVals (f) )
