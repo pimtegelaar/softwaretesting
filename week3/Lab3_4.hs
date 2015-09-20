4b87ab7ff35bb5324d543a3cabf4bfe0a489f26f
weisf_000 <weisf_000@MICROHENK>
2015-09-20 15:25:18 +0200

    Draft for using QuickCheck to verify correctness of CNF formulas compared to regular formulas (not yet via random generator)

--- week3/Lab3_4.hs
+++ week3/Lab3_4.hs
@@ -1,5 +1,7 @@
 module QuickCheckExample where
 import Test.QuickCheck
+import Lecture3
+import Lab3_3
 
 -- Test f commutativity with:
 -- quickCheckResult (\ x y -> f x y == f y x)
@@ -35,3 +37,25 @@ badCompatible x y
  | x == y                = True
  | x == Software_Testing = True
  | otherwise             = False
+
+-- Try with set of fixed formulas to compare truth tables between formulas 
+-- with CNF formulas.
+
+a = Prop 1
+b = Prop 2
+c = Prop 3 
+
+form4 = Impl a b
+form5 = Cnj [a, b]
+form6 = Dsj [a, b]
+form7 = Equiv a b
+form8 = Neg a
+form9 = Prop 1
+form10 = Impl form8 form7
+
+instance Arbitrary Form where
+  arbitrary = elements [form4, form5, form6, form7, form8, form9, form10]
+
+-- Execute Function:
+-- quickCheckResult (\ f -> allVals (cnf (f)) == allVals (f) )
+-- verboseCheck (\ f -> allVals (cnf (f)) == allVals (f) )
