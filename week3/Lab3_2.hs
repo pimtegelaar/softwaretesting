f37c310acf2f08909ca317a673c34bb4ac6a59ff
weisf_000 <weisf_000@MICROHENK>
2015-09-20 22:31:11 +0200

    Added test and provided answers on the Exercise questions

--- week3/Lab3_2.hs
+++ week3/Lab3_2.hs
@@ -3,18 +3,28 @@ module Lab3_2 where
 import Lecture3
 import Testing
 
--- Time spent: 2 hours
--- No time left to implement something more dynamic, 
--- for example of that, see Lab3_4.hs
+-- Time spent:  2 hours
+-- Test method: The test method used is using assertions, the String formula is compared 
+--              with the parse output Form.
+-- Test output: The output of the tests is an empty list, which means it succeeded.
+--              However, no counter examples are generated this way. This way the
+--              correct examples are tested, but the incorrect are ignored. Also
+--              the set of tests is limited. Each time the same Strings are tested.
+--              For some purposes this is good (random tests are not always a good
+--              thing), but in this case it restricts the amount of testcases.
 
 testParse :: (String, [Form]) -> Bool
 testParse (f, g) = parse f == g
 
 -- If the parsing String does not equal the Form assertion, the test fails
-
 parseTests :: [Test]
 parseTests = [ Test "Parse Tests" testParse
              [ ("1+1", [Prop 1])
              , ("(1==>2)", [Impl (Prop 1) (Prop 2)])
+             , ("(3<=>4)", [Equiv (Prop 3) (Prop 4)])
+			 , ("-5", [Neg (Prop 5)])
              ]
            ]
+
+-- Yes, bad naming convention, sorry
+runEverythingInTheWorld = runTests parseTests
