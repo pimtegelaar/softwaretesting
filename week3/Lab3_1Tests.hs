d9977202a219e4d42f46357cfb4610138b3e951f
pimtegelaar <pimtegelaar@gmail.com>
2015-09-20 15:59:26 +0200

    form cannot be a contradiction and satisfiable at the same time

--- week3/Lab3_1Tests.hs
+++ week3/Lab3_1Tests.hs
@@ -8,7 +8,10 @@ instance Arbitrary Form where
   arbitrary = elements [form1,form2]
   
 -- equivalence is independent of order
-testEquiv = quickCheckResult (\ x y -> equiv x y == equiv y x)
+testEquivInAnyOrder = quickCheckResult (\ x y -> equiv x y == equiv y x)
 
 -- form cannot be a contradiction and a tautology at the same time
-testConTaut = quickCheckResult (\ x -> not (contradiction x && tautology x))
+testNotConAndTaut = quickCheckResult (\ x -> not (contradiction x && tautology x))
+
+-- form cannot be a contradiction and satisfiable at the same time
+testNotConAndSat = quickCheckResult (\ x -> not (contradiction x && satisfiable x))
