041b1b21eee4295b0818dccb321165b603d2050e
verhoofstad <verhoofstad@hotmail.com>
2015-09-20 14:09:58 +0200

    Bugfix

--- week3/Lab3_3.hs
+++ week3/Lab3_3.hs
@@ -19,6 +19,6 @@ cnf' :: Form -> Form
 cnf' (Prop x) = Prop x
 cnf' (Neg (Prop x)) = Neg (Prop x)
 cnf' (Dsj [x,(Cnj [y,z])]) = Cnj[cnf'(Dsj[x,y]), cnf'(Dsj[x,z])]   -- Application of the distributive laws
-cnf' (Dsj [Cnj [x,y],z]) = Cnj[cnf'(Dsj[x,y]), cnf'(Dsj[x,z])]     -- to 'push' the disjunctions inwards.
+cnf' (Dsj [Cnj [x,y],z]) = Cnj[cnf'(Dsj[x,z]), cnf'(Dsj[y,z])]     -- to 'push' the disjunctions inwards.
 cnf' (Dsj fs) = Dsj (map cnf' fs)
 cnf' (Cnj fs) = Cnj (map cnf' fs)
