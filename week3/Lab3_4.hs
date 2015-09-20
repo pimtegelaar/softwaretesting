ba03a02766d9af7b23bbb00a37f307e722be6d6e
weisf_000 <weisf_000@MICROHENK>
2015-09-20 18:17:28 +0200

    Trying to make things random

--- week3/Lab3_4.hs
+++ week3/Lab3_4.hs
@@ -1,6 +1,7 @@
 module Lab3_4 where
 
 import Test.QuickCheck
+import System.Random
 import Lecture3
 import Lab3_1
 import Lab3_3
@@ -21,7 +22,11 @@ form9 = Prop 1
 form10 = Impl form8 form7
 
 instance Arbitrary Form where
-  arbitrary = elements [form4, form5, form6, form7, form8, form9, form10]
+  arbitrary = elements [ intToForm1 1 1
+                       , intToForm2 1 2 3, intToForm2 2 4 5
+                       , intToForm2 3 6 7, intToForm2 4 8 9
+					   ]
+--  arbitrary = elements [form4, form5, form6, form7, form8, form9, form10]
 
 -- Compare truth tables for CNF formulas with arbitrary formulas
 quickCheckTruthTable = quickCheckResult (\ f -> allVals (cnf (f)) == allVals (f) )
@@ -42,3 +47,17 @@ checkQuick g h = quickCheckResult (\ f -> g (h (f)) == g (f) )
 -- Verbose version
 checkVerbose :: Eq a => (Form -> a) -> (Form -> Form) -> IO ()
 checkVerbose g h = verboseCheck (\ f -> g (h (f)) == g (f) )
+
+-- Create formulas from Ints
+
+getRandomInt :: Int -> IO Int
+getRandomInt n = getStdRandom (randomR (0,n))
+
+intToForm1 :: Int -> Int -> Form
+intToForm1 a b = Neg (Prop a)
+
+intToForm2 :: Int -> Int -> Int -> Form
+intToForm2 1 a b = Impl (Prop a) (Prop b)
+intToForm2 2 a b = Equiv (Prop a) (Prop b)
+intToForm2 3 a b = Dsj [(Prop a), (Prop b)]
+intToForm2 4 a b = Cnj [(Prop a), (Prop b)]
