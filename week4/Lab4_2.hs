17ccfb67575924f1435d387f55fb9f25e1cef673
softwaregast <weisflog@hotmail.com>
2015-09-26 10:45:12 +0200

    Start with Exercise 4.2

---
+++ week4/Lab4_2.hs
@@ -0,0 +1,32 @@
+module Lab4_2 where
+
+import SetOrd
+import System.Random
+import Test.QuickCheck
+
+-- Time Spent
+
+getRandomInt :: Int -> IO Int
+getRandomInt n = getStdRandom (randomR (0,n))
+
+randomFlip :: Int -> IO Int
+randomFlip x = do 
+   b <- getRandomInt 1
+   if b==0 then return x else return (-x)
+
+genIntList :: IO [Int]
+genIntList = do 
+  k <- getRandomInt 20
+  n <- getRandomInt 10
+  getIntL k n
+
+getIntL :: Int -> Int -> IO [Int]
+getIntL _ 0 = return []
+getIntL k n = do 
+   x <-  getRandomInt k
+   y <- randomFlip x
+   xs <- getIntL k (n-1)
+   return (y:xs)
+
+setInt :: Set Int -> Set Int
+setInt s = s
