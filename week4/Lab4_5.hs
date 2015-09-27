a29da5351c2b04b4c2dc0b7445bc131d237374b2
verhoofstad <verhoofstad@hotmail.com>
2015-09-27 12:18:39 +0200

    Found better solution for symClos.
    The new implementation follows the formal definition more closely.

--- week4/Lab4_5.hs
+++ week4/Lab4_5.hs
@@ -1,12 +1,14 @@
 module Lab4_5 where
 
 import Data.List
-import System.Random
-import Test.QuickCheck
 import SetOrd
+
+-- Time Spent: 1 hour
 
 type Rel a = [(a,a)]
 
+--
+-- Gives the symmetric closure for a given relation.
+--
 symClos :: Ord a => Rel a -> Rel a
-symClos [] = []
-symClos ((x,y):xs) = nub ((x,y):(y,x):symClos xs)
+symClos r = union (nub r) [ (x,y) | (y,x) <- nub r]
