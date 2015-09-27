1d9ac5b74a95760421b66b63390a640228dd0558
verhoofstad <verhoofstad@hotmail.com>
2015-09-27 12:19:33 +0200

    Added comments and an initial implementation.

--- week4/Lab4_6.hs
+++ week4/Lab4_6.hs
@@ -1,16 +1,22 @@
 module Lab4_6 where
 
-import SetOrd
 import Data.List
+import SetOrd
+import Lab4_5
 
--- Time Spent: x hours
-
-type Rel a = [(a,a)]
+-- Time Spent: 2 hours
 
 infixr 5 @@
 
+--
+-- Gives the composition (R o S) of two given relations.
+--
 (@@) :: Eq a => Rel a -> Rel a -> Rel a
 r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
 
+--
+-- Gives the transitive closure (R+) for a given relation
+--
 trClos :: Ord a => Rel a -> Rel a
-trClos a = a
+trClos [] = []
+trClos (x:xs) = r ++ (r @@ r)
