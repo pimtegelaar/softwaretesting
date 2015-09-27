7364f5b859369e962c1f60a25a0d6f1ebc29d549
softwaregast <weisflog@hotmail.com>
2015-09-27 10:54:19 +0200

    Start with Exercise 4 (questions Chapter 5) and Exercise 6 (added initial Functions)

---
+++ week4/Lab4_6.hs
@@ -0,0 +1,16 @@
+module Lab4_6 where
+
+import SetOrd
+import Data.List
+
+-- Time Spent: x hours
+
+type Rel a = [(a,a)]
+
+infixr 5 @@
+
+(@@) :: Eq a => Rel a -> Rel a -> Rel a
+r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
+
+trClos :: Ord a => Rel a -> Rel a
+trClos a = a
