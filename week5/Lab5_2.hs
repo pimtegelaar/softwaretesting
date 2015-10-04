cf0a23f40c96ee59a85eb4e235ac13f4f760051a
verhoofstad <verhoofstad@hotmail.com>
2015-10-04 17:49:13 +0200

    Added generic freeAtPosition function.

--- week5/Lab5_2.hs
+++ week5/Lab5_2.hs
@@ -1,28 +1,20 @@
-module Lab5_1
+module Lab5_2
 
 where 
 
 import Data.List
+import Lecture5
 
-type Row    = Int 
-type Column = Int
-type Value  = Int
 type Position = (Row,Column)
 type Constrnt = [[Position]]
-type Sudoku = (Row,Column) -> Value
-
-positions, values :: [Int]
-positions = [1..9]
-values    = [1..9] 
-
-blocks :: [[Int]]
-blocks = [[1..3],[4..6],[7..9]]
 
 -- The regular constraints for Sudoku can now be stated as:
 
 rowConstrnt = [[(r,c)| c <- values ] | r <- values ]
 columnConstrnt = [[(r,c)| r <- values ] | c <- values ]
 blockConstrnt = [[(r,c)| r <- b1, c <- b2 ] | b1 <- blocks, b2 <- blocks ]
+
+allConstraints = [rowConstrnt,columnConstrnt,blockConstrnt]
 
 -- The generation of the values that are still possible at a given position now takes the following shape:
 
@@ -31,6 +23,14 @@ freeAtPos' s (r,c) xs = let
    ys = filterConstraint (r,c) xs 
  in 
    foldl1 intersect (map ((values \\) . map s) ys)
+   
+freeAtPosition :: Sudoku -> Position -> [Value]
+freeAtPosition s p = freeAtPosition' s p allConstraints   
+   
+freeAtPosition' :: Sudoku -> Position -> [Constrnt] -> [Value]
+freeAtPosition' s p [] = freeAtPos' s p rowConstrnt
+freeAtPosition' s p (c:cs) = (freeAtPos' s p c) `intersect` (freeAtPosition' s p cs)
+   
        
 -- Checks if positions are within the same constraint (block, row, column, etc.)
 sameConstraint :: Position -> Position -> Constrnt -> Bool
@@ -39,3 +39,8 @@ sameConstraint p1 p2 constraint = filterConstraint p1 constraint == filterConstr
 -- Returns only the partition of the constraint the position is in
 filterConstraint :: Position -> Constrnt -> [[Position]]
 filterConstraint p constraint = filter (elem p) constraint
+
+-- Alternative version
+sameblock2 :: Position -> Position -> Constrnt -> Bool
+sameblock2 _ _ [] = False
+sameblock2 p1 p2 (c:xs) = ((p1 `elem` c) && (p2 `elem` c)) || sameblock2 p1 p2 xs
