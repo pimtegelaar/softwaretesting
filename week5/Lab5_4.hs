c7884116b7ccdbd6a084ffd34d24f8e9a86cfb37
softwaregast <weisflog@hotmail.com>
2015-10-10 15:48:30 +0200

    Generate Sudoku with 1 random empty block

--- week5/Lab5_4.hs
+++ week5/Lab5_4.hs
@@ -3,7 +3,7 @@ module Lab5_4
 where 
 
 import Data.List
-import Control.Applicative
+import Control.Monad
 import Lecture5
 
 -- Time spent: 4 hours
@@ -21,32 +21,14 @@ blockConstrnt = [[(r,c)| r <- b1, c <- b2 ] | b1 <- blocks, b2 <- blocks ]
 
 -- Modify generation of problems with additional block parameters
 
-minimalize' :: Node -> [(Row,Column)] -> Node
-minimalize' n [] = n
-minimalize' n ((r,c):rcs) | uniqueSol n' = minimalize' n' rcs
-                          | otherwise    = minimalize' n  rcs
-  where n' = eraseN n (r,c)
-  
-getBlock :: Int -> [(Row,Column)]
-getBlock n = blockConstrnt !! (n - 1)
+getRandomBlocks :: [(Row,Column)] -> IO [(Row,Column)]
+getRandomBlocks c = randomize c
+
+getRandomBlocks' :: [[(Row,Column)]] -> IO [(Row,Column)]
+getRandomBlocks' c = do n <- randomize c
+                        return (n !! 0)
 
 -- Generate a random Sudoku
-
--- getRandomInt :: Int -> IO Int
--- getRandomInt n = getStdRandom (randomR (0,n))
-
--- getRandomItem :: [a] -> IO [a]
--- getRandomItem [] = return []
--- getRandomItem xs = do n <- getRandomInt maxi
-                      -- return [xs !! n]
-                   -- where maxi = length xs - 1
-
--- randomize :: Eq a => [a] -> IO [a]
--- randomize xs = do y <- getRandomItem xs 
-                  -- if null y 
-                    -- then return []
-                    -- else do ys <- randomize (xs\\y)
-                            -- return (head y:ys)
 
 genRandomSudoku' :: IO Node
 genRandomSudoku' = do [r] <- rsolveNs [emptyN]
@@ -55,10 +37,8 @@ genRandomSudoku' = do [r] <- rsolveNs [emptyN]
 randomS' = genRandomSudoku >>= showNode
 
 genProblem' :: Node -> IO Node
--- genProblem' n = do ys <- randomize xs
-genProblem' n = do ys <- randomize . getBlock $ 6
-                   return (minimalize' n ys)
-    -- where xs = filledPositions (fst n) 
+genProblem' n = do ys <- getRandomBlocks' $ blockConstrnt
+                   return (minimalize n ys)
 
 main :: IO ()
 main = do [r] <- rsolveNs [emptyN]
