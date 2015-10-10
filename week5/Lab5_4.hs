d870d0f1cf6d05aa0363766077a847721dff08a1
softwaregast <weisflog@hotmail.com>
2015-10-10 23:26:59 +0200

    Only show the Sudoku Problem when it matches the empty block requirement.

--- week5/Lab5_4.hs
+++ week5/Lab5_4.hs
@@ -4,8 +4,13 @@ where
 
 import Data.List
 import Lecture5
+import Control.Applicative
 
--- Time spent: 7 hours
+-- Time spent: 10 hours
+
+-- Findings: it is still possible to generate a Sudoku with 3 empty blocks, not
+--           with 4 or more. The minimalize function will fail to clear all values,
+--           because it can't guarantee a unique solution anymore.
 
 -- Write a program that generates Sudoku problems with three empty blocks. 
 -- Is it also possible to generate Sudoku problems with four empty blocks? Five? 
@@ -14,6 +19,20 @@ import Lecture5
 -- Deliverables: generator, short report on findings, indication of time spent.
 
 blockConstrnt = [[(r,c)| r <- b1, c <- b2 ] | b1 <- blocks, b2 <- blocks ]
+sampleNode = (grid2sud (example6), constraints (grid2sud (example6)))
+
+-- example
+
+example6 :: Grid
+example6 = [[0,0,0,0,7,0,0,0,0],
+            [0,0,0,1,9,5,0,0,0],
+            [0,0,0,0,0,0,0,6,0],
+            [8,0,0,0,6,0,0,0,3],
+            [4,0,0,8,0,3,0,0,1],
+            [7,0,0,0,2,0,0,0,6],
+            [0,6,0,0,0,0,2,8,0],
+            [0,0,0,4,1,9,0,0,5],
+            [0,0,0,0,8,0,0,7,9]]
 
 -- Modify generation of problems with additional block parameters
 
@@ -33,13 +52,29 @@ genProblem' :: Node -> IO Node
 genProblem' n = do ys <- getRandomBlocks $ blockConstrnt
                    return (minimalize n ys)
 
+genProblem'' :: Node -> IO Node
+genProblem'' n = do ns <- genProblem' n
+                    xs <- checkProblem' ns
+                    if (xs == 3) then 
+                      return (ns)
+                    else
+                      return (emptyN)
+                   
+checkProblem' :: Node -> IO Int
+checkProblem' n = do return (blocksEmpty' n blockConstrnt)
+                   
 main' :: IO ()
 main' = do [r] <- rsolveNs [emptyN]
            showNode r
-           s  <- genProblem' r
+           s  <- genProblem'' r
            showNode s
           
 -- Add checks for verification
+
+-- Same check as below, but for Nodes (usage in IO functions)
+blocksEmpty' :: Node -> [[(Row,Column)]] -> Int
+blocksEmpty' (s, (c:cs)) [] = 0
+blocksEmpty' (s, (_)) (block:blocks) = boolToInt(blockEmpty s block) + (blocksEmpty s blocks)
 
 -- Count the number of empty blocks
 -- Example usage: blocksEmpty (grid2sud example4) blockConstrnt
