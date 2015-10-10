78004487bf4cabccf9badbc08f0d320f811215f4
softwaregast <weisflog@hotmail.com>
2015-10-10 15:51:06 +0200

    Removed obsolete code

--- week5/Lab5_4.hs
+++ week5/Lab5_4.hs
@@ -3,10 +3,9 @@ module Lab5_4
 where 
 
 import Data.List
-import Control.Monad
 import Lecture5
 
--- Time spent: 4 hours
+-- Time spent: 7 hours
 
 -- Write a program that generates Sudoku problems with three empty blocks. 
 -- Is it also possible to generate Sudoku problems with four empty blocks? Five? 
@@ -14,19 +13,13 @@ import Lecture5
 
 -- Deliverables: generator, short report on findings, indication of time spent.
 
-sampleNode = (grid2sud (example1), constraints (grid2sud (example1)))
-sudoku1 = grid2sud(example1)
-sudoku2 = eraseS (sudoku1) (1,1)
 blockConstrnt = [[(r,c)| r <- b1, c <- b2 ] | b1 <- blocks, b2 <- blocks ]
 
 -- Modify generation of problems with additional block parameters
 
-getRandomBlocks :: [(Row,Column)] -> IO [(Row,Column)]
-getRandomBlocks c = randomize c
-
-getRandomBlocks' :: [[(Row,Column)]] -> IO [(Row,Column)]
-getRandomBlocks' c = do n <- randomize c
-                        return (n !! 0)
+getRandomBlocks :: [[(Row,Column)]] -> IO [(Row,Column)]
+getRandomBlocks c = do n <- randomize c
+                       return (n !! 0)
 
 -- Generate a random Sudoku
 
@@ -37,7 +30,7 @@ genRandomSudoku' = do [r] <- rsolveNs [emptyN]
 randomS' = genRandomSudoku >>= showNode
 
 genProblem' :: Node -> IO Node
-genProblem' n = do ys <- getRandomBlocks' $ blockConstrnt
+genProblem' n = do ys <- getRandomBlocks $ blockConstrnt
                    return (minimalize n ys)
 
 main :: IO ()
