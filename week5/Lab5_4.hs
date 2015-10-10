96fcbd066004a75867e3ec70e0588866e076cc6b
softwaregast <weisflog@hotmail.com>
2015-10-10 23:59:50 +0200

    Generator keeps trying until correct amount has been reached.

--- week5/Lab5_4.hs
+++ week5/Lab5_4.hs
@@ -4,25 +4,21 @@ where
 
 import Data.List
 import Lecture5
-import Control.Applicative
 
--- Time spent: 10 hours
+-- Time spent: 11 hours
 
--- Findings: it is still possible to generate a Sudoku with 3 empty blocks, not
---           with 4 or more. The minimalize function will fail to clear all values,
---           because it can't guarantee a unique solution anymore.
-
--- Write a program that generates Sudoku problems with three empty blocks. 
--- Is it also possible to generate Sudoku problems with four empty blocks? Five? 
--- How can you check this?
-
--- Deliverables: generator, short report on findings, indication of time spent.
+-- Findings: a good amount of empty blocks is 3. It is still possible to generate
+--           a Sudoku with 4 empty blocks, but this is much more difficult to find.
+--           The minimalize function will fail in the 4 block case for most
+--           generated problems, because it can't always guarantee a unique solution.
+--           It may run for quite a while. For three empty blocks it is done in a few
+--           tries. We check it now by just generating new problems. If it keeps
+--           running we assume that it is not possible with this amount of empty blocks.
 
 blockConstrnt = [[(r,c)| r <- b1, c <- b2 ] | b1 <- blocks, b2 <- blocks ]
 sampleNode = (grid2sud (example6), constraints (grid2sud (example6)))
 
--- example
-
+-- example with 1 empty block
 example6 :: Grid
 example6 = [[0,0,0,0,7,0,0,0,0],
             [0,0,0,1,9,5,0,0,0],
@@ -34,47 +30,38 @@ example6 = [[0,0,0,0,7,0,0,0,0],
             [0,0,0,4,1,9,0,0,5],
             [0,0,0,0,8,0,0,7,9]]
 
--- Modify generation of problems with additional block parameters
-
+-- Generate 3 random blocks
 getRandomBlocks :: [[(Row,Column)]] -> IO [(Row,Column)]
 getRandomBlocks c = do n <- randomize c
                        return (concat (take 3 n))
 
--- Generate a random Sudoku
-
-genRandomSudoku' :: IO Node
-genRandomSudoku' = do [r] <- rsolveNs [emptyN]
-                      return r
-
-randomS' = genRandomSudoku >>= showNode
-
+-- Generate (random) problems until it matches 3 empty blocks
 genProblem' :: Node -> IO Node
-genProblem' n = do ys <- getRandomBlocks $ blockConstrnt
-                   return (minimalize n ys)
+genProblem' n = do ns <- genRandomProblem n
+                   c <- checkProblem ns
+                   if (c == 3) then 
+                     return (ns)
+                   else
+                     do [r] <- rsolveNs [emptyN]
+                        showNode r
+                        s  <- genProblem' r
+                        return s
 
-genProblem'' :: Node -> IO Node
-genProblem'' n = do ns <- genProblem' n
-                    xs <- checkProblem' ns
-                    if (xs == 3) then 
-                      return (ns)
-                    else
-                      return (emptyN)
-                   
-checkProblem' :: Node -> IO Int
-checkProblem' n = do return (blocksEmpty' n blockConstrnt)
+-- Generate a problem with 3 empty blocks
+genRandomProblem :: Node -> IO Node
+genRandomProblem n = do ys <- getRandomBlocks $ blockConstrnt
+                        return (minimalize n ys)
+                 
+checkProblem :: Node -> IO Int
+checkProblem n = do return (blocksEmptyN n blockConstrnt)
                    
 main' :: IO ()
 main' = do [r] <- rsolveNs [emptyN]
            showNode r
-           s  <- genProblem'' r
+           s  <- genProblem' r
            showNode s
           
 -- Add checks for verification
-
--- Same check as below, but for Nodes (usage in IO functions)
-blocksEmpty' :: Node -> [[(Row,Column)]] -> Int
-blocksEmpty' (s, (c:cs)) [] = 0
-blocksEmpty' (s, (_)) (block:blocks) = boolToInt(blockEmpty s block) + (blocksEmpty s blocks)
 
 -- Count the number of empty blocks
 -- Example usage: blocksEmpty (grid2sud example4) blockConstrnt
@@ -90,3 +77,8 @@ blockEmpty s (p:ps) = s p == 0 && blockEmpty s ps
 boolToInt :: Bool -> Int
 boolToInt False = 0
 boolToInt True  = 1
+
+-- Same check as blocksEmpty, but for Nodes (usage in IO functions)
+blocksEmptyN :: Node -> [[(Row,Column)]] -> Int
+blocksEmptyN (s, (c:cs)) [] = 0
+blocksEmptyN (s, (_)) (block:blocks) = boolToInt(blockEmpty s block) + (blocksEmpty s blocks)
