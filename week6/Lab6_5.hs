e9345b6f197860a5290ace90b60c6d5c4fe1fcbb
softwaregast <weisflog@hotmail.com>
2015-10-13 21:14:24 +0200

    Start with exercise 5

---
+++ week6/Lab6_5.hs
@@ -0,0 +1,13 @@
+module Lab6_5 where
+
+import System.Random
+import Lecture6
+import Lab6_1
+import Lab6_3
+
+carmichael :: [Integer]
+carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) | 
+      k <- [2..], 
+      isPrime (6*k+1), 
+      isPrime (12*k+1), 
+      isPrime (18*k+1) ]
