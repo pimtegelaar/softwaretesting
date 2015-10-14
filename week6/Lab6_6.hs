1720e0bb9d8cd15ad4498b7a2d2b94bea88fb55b
softwaregast <weisflog@hotmail.com>
2015-10-14 22:03:57 +0200

    Draft Miller-Rabin test

---
+++ week6/Lab6_6.hs
@@ -0,0 +1,16 @@
+module Lab6_6 where
+
+import System.Random
+import Lecture6
+import Lab6_1
+import Lab6_3
+import Lab6_4
+import Lab6_5
+
+testMillerRabin :: Integer -> Bool
+testMillerRabin n = True
+
+{--
+ Findings:
+
+--} 
