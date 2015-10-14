06fbc7a181db0140e20ba2b367604112a1c45020
softwaregast <weisflog@hotmail.com>
2015-10-14 21:57:59 +0200

    Attempt to do Fermat test with Carmichael numbers, first findings

--- week6/Lab6_5.hs
+++ week6/Lab6_5.hs
@@ -4,6 +4,7 @@ import System.Random
 import Lecture6
 import Lab6_1
 import Lab6_3
+import Lab6_4
 
 carmichael :: [Integer]
 carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) | 
@@ -11,3 +12,24 @@ carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) |
       isPrime (6*k+1), 
       isPrime (12*k+1), 
       isPrime (18*k+1) ]
+	  
+highComposites :: [Integer]
+highComposites = [ x | x <- [294400..], not (isPrime x) ]
+
+highComposites100 = main 100 (take 10 highComposites)
+highComposites75 = main 75 (take 10 highComposites)
+highComposites50 = main 50 (take 10 highComposites)
+highComposites25 = main 25 (take 10 highComposites)
+highComposites1 = main 1 (take 10 highComposites)
+
+{--
+ Findings:
+ 
+ Via these tests it can be showed that the Fermat test has difficulty
+ with the Carmichael numbers. For a specific test on the range 294400-294410
+ in many attempts the number 294409 will be marked as Prime. The higher k gets,
+ the less times it is indicated. But even with k = 50, it still occurs. So
+ although the chance on mistakes seems to be higher for Carmichael numbers,
+ it does not seem to behave completely different. It still also happens that
+ the test does give correct output. This is (up to a certain degree) random.
+--} 
