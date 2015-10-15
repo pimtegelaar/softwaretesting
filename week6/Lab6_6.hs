6b00301e7e04489457d83b388fc65c6c1d3ef2d1
softwaregast <weisflog@hotmail.com>
2015-10-15 23:10:46 +0200

    Added Miller Rabin tests, issue with multiple Carmichael Numbers

--- week6/Lab6_6.hs
+++ week6/Lab6_6.hs
@@ -7,10 +7,29 @@ import Lab6_3
 import Lab6_4
 import Lab6_5
 
-testMillerRabin :: Integer -> Bool
-testMillerRabin n = True
+testMillerRabin :: Int -> [Integer] -> IO ()
+testMillerRabin _ [] = print "Succeeded"
+testMillerRabin k (c:cs) = do p <- primeMR n c
+                              if p then print ("False - Failed for " ++ show c)
+                              else
+                                testMillerRabin n cs
 
+testMRwithComposites1 = testMillerRabin 1 (take 50 composites)
+testMRwithComposites5 = testMillerRabin 5 (take 50 composites)
+testMRwithComposites10 = testMillerRabin 10 (take 50 composites)
+
+testMRwithCarmichael1 = testMillerRabin 1 (take 5 carmichael)
+testMRwithCarmichael5 = testMillerRabin 5 (take 5 carmichael)
+testMRwithCarmichael10 = testMillerRabin 10 (take 5 carmichael)
+                                
 {--
  Findings:
+ 
+ For Composites the test seems to work properly, especially with higher
+ values for k.
+ 
+ For Carmichael numbers the test has problems. On the moment the
+ test does not finish when giving 5 Carmichael values. Still need
+ to figure out what goes wrong.
 
 --} 
