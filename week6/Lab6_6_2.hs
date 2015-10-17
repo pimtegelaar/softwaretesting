4a9ee98deb2b0df16426c83f4309205362fd48f6
pimtegelaar <pimtegelaar@gmail.com>
2015-10-17 12:58:25 +0200

    Update findings

--- week6/Lab6_6_2.hs
+++ week6/Lab6_6_2.hs
@@ -2,13 +2,10 @@ module Lab6_6_2 where
 
 import System.Random
 import Lecture6
-import Lab6_1
-import Lab6_3
-import Lab6_5
 
 testMillerRabin :: Int -> [Integer] -> IO ()
 testMillerRabin _ [] = print "Succeeded"
-testMillerRabin k (p:ps) = do m <- primeMR k (2^p-1)
+testMillerRabin k (p:ps) = do m <- primeMR k (2^p -1)
                               if m then 
                                 do 
                                   print ("Mersenne: " ++ show p)
@@ -23,5 +20,29 @@ testMersennePrimes = testMillerRabin 1 (take 10000 primes)
  
  At my client this works for first Mersenne Primes, but
  it does not pass 19. Will test further later on.
+ 
+ The speed seems to be caused by the modular exponentiation.
+ When we replace the method in lecture 6 with our alternative, we go up to 4423, before it starts to slow down:
+ 
+ "Mersenne: 2"
+ "Mersenne: 3"
+ "Mersenne: 5"
+ "Mersenne: 7"
+ "Mersenne: 13"
+ "Mersenne: 17"
+ "Mersenne: 19"
+ "Mersenne: 31"
+ "Mersenne: 61"
+ "Mersenne: 89"
+ "Mersenne: 107"
+ "Mersenne: 127"
+ "Mersenne: 521"
+ "Mersenne: 607"
+ "Mersenne: 1279"
+ "Mersenne: 2203"
+ "Mersenne: 2281"
+ "Mersenne: 3217"
+ "Mersenne: 4253"
+ "Mersenne: 4423"
 
 --} 
